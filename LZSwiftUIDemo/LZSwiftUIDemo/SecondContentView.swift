b68df9f0178b24a29b7a2377e2ca2a281e23203d
ZhangZhili <1056522750@qq.com>
2022-01-24 14:19:52 +0800

    swiftUI

---
+++ LZSwiftUIDemo/LZSwiftUIDemo/SecondContentView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondContentView.swift
+//  LZSwiftUIDemo
+//
+//  Created by lizhi on 2022/1/24.
+//
+
+import SwiftUI
+
+struct SecondContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondContentView()
+    }
+}
