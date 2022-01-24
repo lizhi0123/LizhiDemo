b61062dbb2e666ab8e0f3f70b0196dd77776c898
ZhangZhili <1056522750@qq.com>
2022-01-24 13:19:27 +0800

    swiftUI

---
+++ LZSwiftUIDemo/LZSwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LZSwiftUIDemo
+//
+//  Created by lizhi on 2022/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LZSwiftUIDemo/LZSwiftUIDemo/LZSwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LZSwiftUIDemoApp.swift
+//  LZSwiftUIDemo
+//
+//  Created by lizhi on 2022/1/24.
+//
+
+import SwiftUI
+
+@main
+struct LZSwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
